93cee17987d012704e9fe08433cbda277e053737
Hieracosphynx <cortezmiguel05@gmail.com>
2021-07-16 15:50:00 -0700

    Css style changes for weather component.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,9 +2,10 @@ import Weather from './Weather';
 import Card from '../UI/Card';
 
 const Content = (props) => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
   return (
     <Card>
-      <Weather />
+      <Weather api_key={API_KEY} />
     </Card>
   );
 };
--- src/components/Content/Weather.js
+++ src/components/Content/Weather.js
@@ -4,10 +4,9 @@ import classes from './Weather.module.css';
 
 const Weather = (props) => {
   const [localWeather, setLocalWeather] = useState([]);
-  const API_KEY = process.env.REACT_APP_API_KEY;
   const showLocalWeatherHandler = useCallback(() => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Las%20Vegas,Nevada&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=Las%20Vegas,Nevada&appid=${props.api_key}`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -15,15 +14,15 @@ const Weather = (props) => {
           id: data.weather[0].id,
           weather: data.weather[0].main,
           description: data.weather[0].description,
-          current_temp: convertTemp(data.main.temp.toFixed(2)),
-          hi_temp: convertTemp(data.main.temp_max.toFixed(2)),
+          current_temp: convertTemp(data.main.temp).toFixed(2),
+          hi_temp: convertTemp(data.main.temp_max).toFixed(2),
         };
         setLocalWeather(weather);
       })
       .catch((e) => {
         console.log(e);
       });
-  }, [API_KEY]);
+  }, [props.api_key]);
 
   const convertTemp = (temp) => {
     return ((temp - 273.15) * 9) / 5 + 32;
@@ -34,23 +33,31 @@ const Weather = (props) => {
   }, [showLocalWeatherHandler]);
 
   return (
-    <div>
-      <span>
-        <h4>Weather:</h4>
-        <p>{localWeather.weather}</p>
-      </span>
-      <span>
-        <h4>Description:</h4>
-        <p>{localWeather.description}</p>
-      </span>
-      <span>
-        <h4>Current Temperature:</h4>
-        <p>{localWeather.current_temp} °F</p>
-      </span>
-      <span>
-        <h4>High:</h4>
-        <p>{localWeather.hi_temp} °F</p>
-      </span>
+    <div className={classes['weather-container']}>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>Weather: </b>
+          {localWeather.weather}
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>Description: </b>
+          {localWeather.description}
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>Current Temperature: </b>
+          {localWeather.current_temp} °F
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>High: </b>
+          {localWeather.hi_temp} °F
+        </span>
+      </div>
     </div>
   );
 };
