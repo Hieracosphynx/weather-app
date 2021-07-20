fb1ca9f42db86039fef7b220a7c2a835bc7ccd2e
Hieracosphynx <cortezmiguel05@gmail.com>
2021-07-19 18:06:25 -0700

    Weather app covert metric

--- src/components/Content/Weather.js
+++ src/components/Content/Weather.js
@@ -1,88 +1,78 @@
 import React, { useEffect, useCallback, useState } from 'react';
-import spinner from '../../assets/spinner.gif';
 import classes from './Weather.module.css';
 
 const Weather = (props) => {
-  const [localWeather, setLocalWeather] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { localWeather, onCelsius, onFahrenheit, isFahrenheit } = props;
 
-  const showLocalWeatherHandler = useCallback(() => {
-    setIsLoading(true);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Las%20Vegas,Nevada&appid=${props.api_key}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const weather = {
-          id: data.weather[0].id,
-          weather: data.weather[0].main,
-          humidity: data.main.humidity,
-          current_temp: convertTemp(data.main.temp).toFixed(0),
-          hi_temp: convertTemp(data.main.temp_max).toFixed(0),
-        };
-        setLocalWeather(weather);
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        console.log(e);
-        setError(e);
-      });
-  }, [props.api_key]);
-
-  const convertTemp = (temp) => {
-    return ((temp - 273.15) * 9) / 5 + 32;
+  const onCelsiusHandler = () => {
+    onCelsius({
+      current_temp: localWeather.current_temp,
+      hi_temp: localWeather.hi_temp,
+    });
   };
 
-  useEffect(() => {
-    showLocalWeatherHandler();
-  }, [showLocalWeatherHandler]);
+  const onFahrenheitHandler = () => {
+    onFahrenheit({
+      current_temp: localWeather.current_temp,
+      hi_temp: localWeather.hi_temp,
+    });
+  };
+  let metric = '°F';
 
-  let content = (
-    <div className={classes['weather-container__temp']}>
-      <img src={spinner} alt='Loading...' />
-    </div>
-  );
-
-  if (!isLoading && !error) {
-    content = (
-      <React.Fragment>
-        <div className={classes['weather-container__temp']}>
-          <span>
-            <h3>{localWeather.current_temp}°F</h3>
-          </span>
-        </div>
-        <div className={classes['weather-container__controls']}>
-          <span>
-            <b>High: </b>
-            {localWeather.hi_temp}°F
-          </span>
-        </div>
-        <div className={classes['weather-container__controls']}>
-          <span>
-            <b>Weather: </b>
-            {localWeather.weather}
-          </span>
-        </div>
-        <div className={classes['weather-container__controls']}>
-          <span>
-            <b>Humidity: </b>
-            {localWeather.humidity}%
-          </span>
-        </div>
-      </React.Fragment>
-    );
+  if (isFahrenheit) {
+    metric = '°F';
+  }
+  if (!isFahrenheit) {
+    metric = '°C';
   }
 
-  if (error && isLoading) {
-    content = (
-      <div className={classes['weather-container__temp']}>
-        <b>{error}</b>
+  return (
+    <React.Fragment>
+      <div className={classes.metric}>
+        <button onClick={onFahrenheitHandler}>
+          {isFahrenheit ? (
+            <b className={classes['current-metric']}>°F</b>
+          ) : (
+            '°F'
+          )}
+        </button>
+        <button onClick={onCelsiusHandler}>
+          {!isFahrenheit ? (
+            <b className={classes['current-metric']}>°C</b>
+          ) : (
+            '°C'
+          )}
+        </button>
       </div>
-    );
-  }
-
-  return <div className={classes['weather-container']}>{content}</div>;
+      <div className={classes['weather-container__temp']}>
+        <span>
+          <h3>
+            {localWeather.current_temp}
+            {metric}
+          </h3>
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>High: </b>
+          {localWeather.hi_temp}
+          {metric}
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>Weather: </b>
+          {localWeather.weather}
+        </span>
+      </div>
+      <div className={classes['weather-container__controls']}>
+        <span>
+          <b>Humidity: </b>
+          {localWeather.humidity}%
+        </span>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Weather;
